224831017e3149de85df0da06fd3c8cf765c6f72
Steven Barnes <stbarnes@millersville.edu>
2024-01-29 19:25:01 -0500

    exports router

--- lab01/routes/biography.js
+++ lab01/routes/biography.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET biography page */
 router.get('/', function (req, res, next) {
     res.render('biography', {title:"Biography"})
 })
+
+module.exports = router;
