675eb50af19d5e260ca553f7e847227b9fd83945
Steven Barnes <stbarnes@millersville.edu>
2024-02-05 19:34:21 -0500

    adds blog controller

---
+++ blogger/app_server/controllers/blog.js
@@ -0,0 +1,7 @@
+module.exports.blogList = function(req, res) {
+    res.render('blog/blog-list', { title: 'Blog List' })
+}
+
+module.exports.blogAdd = function(req, res) {
+    render.render('blog/blog-add', { title: 'Blog Add' })
+}
