ca4f74eb0b9dbc2cd3bbd22fd52511fc8b4a7df8
Steven Barnes <stbarnes@millersville.edu>
2024-02-12 19:33:28 -0500

    updates view to render blogs

--- blogger/app_server/controllers/blog.js
+++ blogger/app_server/controllers/blog.js
@@ -1,5 +1,32 @@
+var second = 1000;
+var minute = second * 60;
+var hour = minute * 60;
+var day = hour * 24;
+var dtNow = Date.now();
+
+var blogs = [
+    {
+        blogTitle: "My first blog!",
+        blogText: "This is my first blog.",
+        createdOn: new Date(dtnow - (day*(dtNow % 13)))
+    },
+    {
+        blogTitle: "A second blog.",
+        blogText: "The second blog shows how we iterate an array of objects using a pug template.",
+        createdOn: new Date(dtnow - (hour*(dtNow % 15)))
+    },
+    {
+        blogTitle: "Now we're getting MEAN!",
+        blogText: "Being MEAN is a big change for me.  I'm use to being as nice as I can!",
+        createdOn: new Date(dtnow - (minute*(dtNow % 19)))
+    }
+]
+
 module.exports.blogList = function(req, res) {
-    res.render('blog/blog-list', { title: 'Blog List' })
+    res.render('blog/blog-list', {
+        title: 'Blog List',
+        blogs: blogs
+    });
 }
 
 module.exports.blogAdd = function(req, res) {
