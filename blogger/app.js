ea44f1636e1900817b9e731cc68b7fbea431cdf6
Steven Barnes <stbarnes@millersville.edu>
2024-02-17 02:06:33 -0500

    corrects typo

--- blogger/app.js
+++ blogger/app.js
@@ -18,8 +18,8 @@ app.set('view engine', 'pug');
 
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
-app.use('/icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/fonts')))
+// app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
+app.use('/icons', express.static(path.join(__dirname, 'node_modules/bootstrap-icons/font')))
 
 app.use(logger('dev'));
 app.use(express.json());
