f96c9c44ddd690b3a7cc3d36f7a6a2c3aec32838
Steven Barnes <stbarnes@millersville.edu>
2024-02-21 17:15:40 -0500

    update router methods

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -6,6 +6,6 @@ var ctrlBlogs = require('../controllers/blog');
 router.get('/blog', ctrlBlogs.blogList);
 router.post('/blog/add', ctrlBlogs.blogAdd);
 router.get('/blog/:blogId', ctrlBlogs.blogFindOne);
-router.put('/blog/:blogId', ctrlBlogs.blogEdit);
+router.post('/blog/:blogId', ctrlBlogs.blogEdit);
 
 module.exports = router;
