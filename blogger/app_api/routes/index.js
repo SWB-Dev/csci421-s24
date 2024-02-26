aa765f657bcfd9b7f1b02b581da011e8ed3d6da9
Steven Barnes <stbarnes@millersville.edu>
2024-02-26 18:21:22 -0500

    use put for an edit

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -6,7 +6,7 @@ var ctrlBlogs = require('../controllers/blog');
 router.get('/blog', ctrlBlogs.blogList);
 router.post('/blog/add', ctrlBlogs.blogAdd);
 router.get('/blog/:blogId', ctrlBlogs.blogFindOne);
-router.post('/blog/:blogId', ctrlBlogs.blogEdit);
+router.put('/blog/:blogId', ctrlBlogs.blogEdit);
 router.delete('/blog/:blogId', ctrlBlogs.blogDelete);
 
 module.exports = router;
