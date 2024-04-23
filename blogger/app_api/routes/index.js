576386d8ddd47498a0d585cdfb6f18e07576934d
SWB <steven.barnes@swbdevelopment.com>
2024-04-23 18:52:15 -0400

    adds pictionary endpoints

--- blogger/app_api/routes/index.js
+++ blogger/app_api/routes/index.js
@@ -7,6 +7,7 @@ var auth = jwt({
 });
 
 var ctrlBlogs = require('../controllers/blog');
+var ctrlPictionary = require('../controllers/pictionary');
 var ctrlAuth = require('../controllers/authentication');
 
 router.get('/blog', ctrlBlogs.blogList);
@@ -16,5 +17,19 @@ router.put('/blog/:blogId', auth, ctrlBlogs.blogEdit);
 router.delete('/blog/:blogId', auth, ctrlBlogs.blogDelete);
 router.post('/register', ctrlAuth.register);
 router.post('/login', ctrlAuth.login);
+
+// PICTIONARY
+router.get('/pictionary', ctrlPictionary.getPictionary);
+router.post('/pictionary/create', auth, ctrlPictionary.createPictionary);
+router.post('/pictionary/connectUser', auth, ctrlPictionary.connectUser);
+router.post('/pictionary/createRound', auth, ctrlPictionary.createRound);
+router.post('/pictionary/startRound', auth, ctrlPictionary.startRound);
+router.get('/pictionary/getGameTick', auth, ctrlPictionary.getGameTick);
+router.delete('/pictionary/clearGame', auth, ctrlPictionary.clearGame);
+router.post('/pictionary/addWords', auth, ctrlPictionary.addWords);
+router.post('/pictionary/endRound', auth, ctrlPictionary.endRound);
+router.post('/pictionary/submitGuess', auth, ctrlPictionary.submitGuess);
+router.post('/pictionary/updateDrawImage', auth, ctrlPictionary.updateDrawImage);
+router.delete('/pictionary/disconnectUser', auth, ctrlPictionary.disconnectUser);
 
 module.exports = router;
